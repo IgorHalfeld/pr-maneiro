021e899fd7a4b487ee4f03052f9eb9be8acf328f
Fabricio Augusto <fabricionaweb@gmail.com>
2019-11-01 09:13:57 +0000

    apply prettier

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-;(async () => {
+(async () => {
   const loadComponent = async component => {
     const res = await fetch('/components/' + component + '.html');
     return await res.text();
@@ -22,19 +22,26 @@
   };
 
   const checkCopyFeature = () => {
-    if(!navigator.clipboard || !navigator.clipboard.write || typeof ClipboardItem === 'undefined' ){
+    if (
+      !navigator.clipboard ||
+      !navigator.clipboard.write ||
+      typeof ClipboardItem === 'undefined'
+    ) {
       buttonCopy.remove();
     }
   };
-  const replaceValues = (event) => {
+
+  const replaceValues = event => {
     const defaultValue = template.msg;
-    const value = !!event && event.target.value.length ? event.target.value : defaultValue;
+    const value =
+      !!event && event.target.value.length ? event.target.value : defaultValue;
 
     const tweetComplete = tweetView
-      .replace('{{tweetContent}}', value)
-      .replace('{{name}}', template.name)
-      .replace('{{username}}', template.username)
-      .replace('{{image}}', template.image);
+      .replace('{{ tweetContent }}', value)
+      .replace('{{ name }}', template.name)
+      .replace('{{ username }}', template.username)
+      .replace('{{ image }}', template.image);
+
     target.innerHTML = tweetComplete;
   };
 
@@ -42,7 +49,7 @@
     const dataUrl = await domtoimage.toBlob($('#preview'), { quality: 0.95 });
     const item = new ClipboardItem({ 'image/png': dataUrl });
     navigator.clipboard.write([item]);
-  }
+  };
 
   const handleDownload = async () => {
     const dataUrl = await domtoimage.toPng($('#preview'), { quality: 0.95 });
@@ -50,12 +57,13 @@
     link.download = 'pr-maneiro.png';
     link.href = dataUrl;
     link.click();
-  }
+  };
 
   buttonCopy.addEventListener('click', handleCopy);
   buttonDownload.addEventListener('click', handleDownload);
   templateElement.addEventListener('change', selectTemplate);
   tweetValue.addEventListener('keyup', replaceValues);
+
   replaceValues();
   checkCopyFeature();
-})()
+})();
