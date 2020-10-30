ea2a245335b82c12569e4076014ce68f5687656e
Marcos Felipe <omarkdev@gmail.com>
2020-10-30 14:59:44 -0300

    Fix: Manter tweet digitado ao trocar de tweet

--- src/App.vue
+++ src/App.vue
@@ -121,7 +121,10 @@ export default defineComponent({
       currentIndex = currentIndex >= persons.length - 1 ? 0 : currentIndex += 1
       const tweet = persons[currentIndex]
       animateNext(store.tweetRef, {
-        onExitFinish: () => setCurrentTweet(tweet)
+        onExitFinish: () => setCurrentTweet({
+          ...tweet,
+          msg: store.search.length ? store.search : tweet.msg
+        })
       })
     }
 
@@ -129,7 +132,10 @@ export default defineComponent({
       currentIndex = currentIndex <= 0 ? persons.length - 1 : currentIndex -= 1
       const tweet = persons[currentIndex]
       animatePrevious(store.tweetRef, {
-        onExitFinish: () => setCurrentTweet(tweet)
+        onExitFinish: () => setCurrentTweet({
+          ...tweet,
+          msg: store.search.length ? store.search : tweet.msg
+        })
       })
     }
 
