7d60ef2ffb50575c50cb471c91b5937217f796c0
Marco Túlio Terra <marcotulio.mtt@gmail.com>
2020-10-30 00:17:49 -0400

    Atualizado assets do relacionado a pwa (#84)

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,35 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
 module.exports = {
   pwa: {
     name: 'PR Maneiro',
     themeColor: '#27A1F9',
-    msTileColor: '#000000',
+    msTileColor: '#FFF',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'white'
+    appleMobileWebAppStatusBarStyle: 'white',
+    manifestOptions: {
+      background_color: '#FFF',
+      icons: [
+        {
+          src: 'img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable any'
+        },
+        {
+          src: 'img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable any'
+        }
+      ]
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+      maskIcon: null
+    }
   }
 }
